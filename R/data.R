46667156fb8acae7fc23dd2342219198b210767a
Micah Altman <micah.altman@gmail.com>
2023-12-18 19:43:13 -0500

    adding country codes

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' ISO 3166 country codes
+#'
+#' Country codes and names
+#'
+#' @format ## `iso3167`
+#' A data frame 252 observation and 3 columns
+#' \describe{
+#'   \item{country_name}{Country name}
+#'   \item{country_2d}{2 letter ISO country codes}
+#'   \item{country_num}{country numeric code}
+#'   ...
+#' }
+#' @source <https://en.wikipedia.org/wiki/ISO_3166-2>
+"iso3166"
